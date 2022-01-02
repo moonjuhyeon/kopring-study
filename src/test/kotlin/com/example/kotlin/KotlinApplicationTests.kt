acc3cc77e96c4ccf0c547787681bc87c2a4c9eb5
moon <hoopster@airi.kr>
2022-01-02 13:44:03 +0900

    chore : logging config setup

--- src/test/kotlin/com/example/kotlin/KotlinApplicationTests.kt
+++ src/test/kotlin/com/example/kotlin/KotlinApplicationTests.kt
@@ -2,9 +2,7 @@ package com.example.kotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootTest
 class KotlinApplicationTests {
 
